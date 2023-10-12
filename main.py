59a2eaaddc75814be6ad3d5a19c956140ed9d268
Jaison Philip <jaisonvellinchayil@gmail.com>
2023-10-12 20:14:50 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,76 @@
+import streamlit as st
+from streamlit_chat import message
+from langchain.chains import ConversationalRetrievalChain
+from langchain.document_loaders import CSVLoader, DirectoryLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.llms import CTransformers
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+
+#load the pdf from the path
+loader = DirectoryLoader(path='data/',glob="*.csv",loader_cls=CSVLoader)
+documents = loader.load()
+
+#splitting the loaded documents
+text_splitter  = RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap=50)
+text_chunks = text_splitter.split_documents(documents)
+
+#creating embeddings
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",
+                                   model_kwargs={'device':"cpu"})
+
+#vectorstore
+vector_store = FAISS.from_documents(text_chunks,embeddings)
+
+#create llm
+llm = CTransformers(model="TheBloke/Llama-2-7B-Chat-GGML",model_type="llama",
+                    config={'max_new_tokens':128,'temperature':0.01})
+
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+chain = ConversationalRetrievalChain.from_llm(llm=llm,chain_type='stuff',
+                                              retriever=vector_store.as_retriever(search_kwargs={"k":2}),
+                                              memory=memory)
+
+st.title("Road Master 👮")
+def conversation_chat(query):
+    result = chain({"question": query, "chat_history": st.session_state['history']})
+    st.session_state['history'].append((query, result["answer"]))
+    return result["answer"]
+
+def initialize_session_state():
+    if 'history' not in st.session_state:
+        st.session_state['history'] = []
+
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = ["Hello! Ask me anything about traffic rules 🤗"]
+
+    if 'past' not in st.session_state:
+        st.session_state['past'] = ["Hey! 👋"]
+
+def display_chat_history():
+    reply_container = st.container()
+    container = st.container()
+
+    with container:
+        with st.form(key='my_form', clear_on_submit=True):
+            user_input = st.text_input("Question:", placeholder="Drop your query", key='input')
+            submit_button = st.form_submit_button(label='Send')
+
+        if submit_button and user_input:
+            output = conversation_chat(user_input)
+
+            st.session_state['past'].append(user_input)
+            st.session_state['generated'].append(output)
+
+    if st.session_state['generated']:
+        with reply_container:
+            for i in range(len(st.session_state['generated'])):
+                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="thumbs")
+                message(st.session_state["generated"][i], key=str(i), avatar_style="fun-emoji")
+
+# Initialize session state
+initialize_session_state()
+# Display chat history
+display_chat_history()
